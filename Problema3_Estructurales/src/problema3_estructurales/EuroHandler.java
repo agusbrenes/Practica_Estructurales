f449324ab0bcffe903e32d232e688a3dab22a4a0
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-05-16 19:41:18 -0600

    Update EuroHandler.java

--- Problema3_Estructurales/src/problema3_estructurales/EuroHandler.java
+++ Problema3_Estructurales/src/problema3_estructurales/EuroHandler.java
@@ -5,9 +5,7 @@ public class EuroHandler {
     public EuroHandler() {}
 
     public void processEuros(double amount) {
-        //String euros = Double.toString(amount);
         System.out.println(amount + " euros han sido depositados!");
-        //return Double.toString(amount);
     }
 
 }
