06789cad05edcf0be879fd75c836a08284d49b85
Exotronic <ecotro2014gd@gmail.com>
2021-05-17 21:00:50 -0600

    dasdasda

--- Practica_Estructurales/src/probema1/Probema1_Estructurales.java
+++ Practica_Estructurales/src/probema1/Probema1_Estructurales.java
@@ -1,9 +1,14 @@
 package probema1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Probema1_Estructurales {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello World!");
+        
+        File xmlFile = new File("data.xml");
+        Scanner myScanner = new Scanner(xmlFile);
     }
-
 }
